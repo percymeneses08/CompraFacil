6caa224a3fd9fc68856267ee247942c6f51c7bd7
PBenavides <fob534154@gmail.com>
2019-07-23 15:07:20 -0500

    metro scrap

---
+++ scraping/metro_scrap.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+import re
+
+url2 = "https://www.metro.pe/lacteos"
+response2 = requests.get(url2) #conexion
+soup2 = BeautifulSoup(response2.text, "html.parser") #Haciendo el soup Object
+div_principal = soup2.find('div', {'class':'main'}) #Busco en la clase main
+
+#Según el tutorial en stackoverflow
+#Lo meteremos a una lista
+links_leche = []
+for div in div_principal.find_all("div", {"class":"product-item__info"}): #Encuentra todas las clases de info de producto
+    for link in div.select("a.product-item__name"): #Seleccioname los links
+        print(link['href'])
+        links_leche.append(link['href'])
+        
+def scrapeando_ando(url): #generalizamos el jalado de data
+    response = requests.get(url) 
+    soup = BeautifulSoup(response.text, "html.parser")
+    nombre_ = soup.find('div', {'class': 'name-mobile mobile'})
+    nombre3 = nombre_.div(string=True)
+    precio_ = soup.find('div', {'class':'plugin-preco'})
+    precio_2 = precio_.find('strong', {'class':'skuBestPrice'}).text
+    return nombre3[0], precio_2
+
+for links in links_leche:
+    scrapeando_ando(links)
