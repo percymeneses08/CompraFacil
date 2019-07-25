3b5218dd663f30f6dab489477b56d808e33a1320
PBenavides <fob534154@gmail.com>
2019-07-25 15:27:47 -0500

    last actualization

--- scraping/metro_scrap.py
+++ scraping/metro_scrap.py
@@ -43,3 +43,45 @@ df['Supermercado'] = 'Metro'
 df['Precio_float'] = df.Price.str.extract('(\d+\.\d+)')
 
 export = df.to_json('export_dataframe.json')
+
+print('-------------------------------Otra Alternativa------------------------------------------------')
+
+def agrega_x_num(num):
+    x = 1
+    lista_de_urls = []
+    for i in range(1,num):
+        x = str(x)
+        url_conjunto = 'https://www.metro.pe/buscapagina?sl=19ccd66b-b568-43cb-a106-b52f9796f5cd&PS=18&cc=18&sm=0&PageNumber='+x+'&&fq=C%3a%2f1001436%2f'
+        lista_de_urls.append(url_conjunto)
+        x = int(x)
+        x+=1
+    return lista_de_urls
+
+lista_de_25 = agrega_x_num(26)
+
+#Tengo que ubicar dónde está la info: los datos que busco son nombre, precio regular, marca, codigo de producto, precio online.
+#Buscaremos primero el nombre y la marca. Lo formalizaremos en una función para luego aplicarle un for loop a los links
+lista_nombres = []
+lista_marcas = []
+def get_names_and_brands(lista_links):
+    global lista_nombres
+    global lista_marcas
+    for link in lista_links:
+        response = requests.get(link) #Conecto el link
+        soup = BeautifulSoup(response.text,"html.parser") #Hago el objeto
+        nombres_del_link = [div_prods.find('a',{'class':'product-item__name'}).text #Esto me botará una lista con los nombres
+                  for div_prods in soup.find_all('div',{"class":"product-item__info"})] 
+        marcas_del_link = [div_prods.find('div',{'class':'product-item__brand'}).text #Esto me botará una lista con las marcas
+                for div_prods in soup.find_all('div',{"class":"product-item__info"})]
+        lista_nombres = lista_nombres + nombres_del_link
+        lista_marcas = lista_marcas + marcas_del_link
+
+
+##EJECUTO LA FUNCION
+
+get_names_and_brands(lista_de_25)        
+        
+## Las variables están en dos columnas, lista_nombres y lista_marcas
+
+
+## AUN FALTA SACAR LOS PRECIOS
