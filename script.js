9697b0b7133c88ecf11f2a47355feea6a56bccca
jfacabreran <jfacabreran@gmail.com>
2019-07-25 23:20:05 -0500

    cambio de resultados

---
+++ script.js
@@ -0,0 +1,5 @@
+var superW = document.getElementById("wong").children[1].children[1].children
+var superM = document.getElementById("metro").children[1].children[1].children
+var superT = document.getElementById("tottus").children[1].children[1].children
+
+
